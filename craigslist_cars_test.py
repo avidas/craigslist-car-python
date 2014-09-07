2251a2d99710da81b2d4b1eb734ddd27cbb011d6
Avi Das <adas@colgate.edu>
2014-09-06 19:04:53 -0500

    Start adding tests for the helper methods

---
+++ craigslist_cars_test.py
@@ -0,0 +1,27 @@
+import cragislist_cars
+import unittest
+import os
+
+
+class CraigslistCarsTestCase(unittest.TestCase):
+
+    """Test Cases"""
+
+    def test_parse_car_conditions(self):
+        """Ensure car conditions are parsed to return defaultdict"""
+        pass
+
+    def test_parse_car_listing(self):
+        """Test car details from craigslist parsed successfully"""
+        pass
+
+    def test_parse_car_posted(self):
+        """Check date parsed in expected format"""
+        pass
+
+    def test_get_craigslist_cars(self):
+        """Test that given city, limit, brand, model, price, list of car entries returned"""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
